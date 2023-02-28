d1219e4cc0ad501f9da9740c62cf3bca562b1f48
Aybarinee Campbell Mendez <Aybarineecampbellmendez@gmail.com>
2023-02-28 18:12:23 -0500

    added up to ex 6

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -19,7 +19,7 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/about/')
+@app.route('/about/', methods=['GET','POST'])
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
